51b66a43f450bc70d236679ec9c065dca8ea96a5
Leo <>
2023-04-04 00:27:53 +0200

    Create placeholder test

---
+++ src/test/java/PlaceHolder.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PlaceHolder {
+
+    @Test
+    public void placeHolder(){
+        Assertions.assertTrue(true);
+    }
+}
